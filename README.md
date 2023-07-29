0051d09108a7630c62b6020daa7b2fafa9940ab4
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-07-29 15:14:56 -0400

    implementa combobox headless manufacturers

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 https://www.youtube.com/watch?v=pUNSHPyVryU&t=75s
 
 <img src="image.png" alt="crud react" width="400">
+
+- React
+- NextJS 13
+- TypeScript
+- Tailwind
+- @headlessui/react
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacture from "./SearchManufacturer";
 
-export { Hero, CustomButton, NavBar, Footer };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacture,
+};
