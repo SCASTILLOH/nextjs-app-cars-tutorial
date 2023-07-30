35f7848535b9c2c5cdebec303fb24424016f1b58
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-07-30 11:50:47 -0400

    implementa filter

--- utils/index.ts
+++ utils/index.ts
@@ -35,3 +35,13 @@ export async function fetchCars(filters: FilterProps) {
 }
 
 export const generateCarImageUrl = (car: CarProps, angel?: string) => {};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathName;
+};
